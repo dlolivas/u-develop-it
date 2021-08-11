4ed2b7a3e0da909a47b34d4c937910fb724a2512
Diana Olivas <dianaolivas181@gmail.com>
2021-08-10 23:32:42 -0500

    added voters table

--- server.js
+++ server.js
@@ -54,206 +54,200 @@ const db = mysql.createConnection(
 });
 */
 
-//API ROUTES FOR ALL PARTIES TABLE
-app.get('/api/parties' , (req, res) => {
-    const sql = ` SELECT * FROM parties `;
-    db.query(sql, (err, rows) => {
-        if (err) {
-            res.status(500).json({ error: err.message });
-            return;
-        }
-        res.json({
-            message: 'success' , 
-            data: rows
-        });
-    });
-});
-
-
-//API ROUTES FOR  A SINGLE PARTY WITH ID PARAMETER
-app.get('/api/party/:id' , (req, res) => {
-    const sql = `SELECT * FROM parties WHERE id = ? `;
-    const params = [req.params.id];
-    db.query(sql, params, (err, row) => {
-        if (err) {
-            res.status(400).json({ error: err.message });
-            return;
-        }
-        res.json({
-            message: 'success',
-            data: row
-        });
-    });
-});
-
-// get ALL CANDIDATES with get routes 
-app.get('/api/candidates' , (req, res) => {//api endpoint
-    const sql = `SELECT candidates. * , parties.name
-                AS party_name
-                FROM candidates
-                LEFT JOIN parties
+// Get all candidates and their party affiliation
+app.get('/api/candidates', (req, res) => {
+  const sql = `SELECT candidates.*, parties.name 
+                AS party_name 
+                FROM candidates 
+                LEFT JOIN parties 
                 ON candidates.party_id = parties.id`;
-
-    db.query(sql, (err, rows) => {
-        if (err) {
-            res.status(500).json({ error: err.message });// instead of an err we'll send a status code of 500 and place the err msj within JSON obj
-            return;
-        }
-        res.json({// if there is not an err the response will be sent back saying sucess
-            message: 'success',
-            data: rows
-        });
-    });
-});
-
-//delete party route
-app.delete('/api/party/:id', (req, res) => {
-    const sql = `DELETE FROM parties WHERE id = ?`;
-    const params = [req.params.id];
-    db.query(sql, params, (err, result) => {
-      if (err) {
-        res.status(400).json({ error: res.message });
-        // checks if anything was deleted
-      } else if (!result.affectedRows) {
-        res.json({
-          message: 'Party not found'
-        });
-      } else {
-        res.json({
-          message: 'deleted',
-          changes: result.affectedRows,
-          id: req.params.id
-        });
-      }
-    });
-});
-
-
-
-//GET A SINGLE CANDIDATE WITH API ENDPOINT
-app.get('/api/candidate/:id' , (req, res) => {//api endpoint has an :id which specify which candidate we'll select from the database
-    const sql = `SELECT candidates.*, parties.name 
-              AS party_name 
-              FROM candidates 
-              LEFT JOIN parties 
-              ON candidates.party_id = parties.id 
-             WHERE candidates.id = ?`;; // mention id 
-    const params = [req.params.id];// we'll assign the captured value populated in the re.params obj with the key id to params. Params is assigned as an array with a single element req.params.id
-
-    db.query(sql, params, (err, row) => {// the database then query the candidates table with this id and retrive the row specififed bcuz params can be accepted in the database call as an array. 
-        if (err) {
-            res.status(400).json({error: err.message });
-            return;
-        }
-        res.json({
-            message: 'success' ,
-            data: row
-        });
-
-    });
-});
-
-
-
-// DELETE A CANDIDATE WITH API ENDPOINT 
-app.delete('/api/candidate/:id', (req, res) => { //api endpoint
-    const sql = `DELETE FROM candidates WHERE id = ? ` ; // we are assigning the sql to delete from candidates table , where do we want to delete it by the id
-    const params = [req.params.id];
-
-    db.query(sql, params, ( err, result) => {
-        if (err) {
-            res.statusMessage(400).json({ error: res.message });
-        } else if (!result.affectedRows) { // if user wants to delete  a candidate that does not exist no affectedRows as a result of the delete query means that there was no candidate by that id
-            res.json({
-                message: 'Candidate not found'
-            });
-        } else {
-            res.json({
-                message: 'deleted' , 
-                changes: result.affectedRows,
-                id: req.params.id
-            });
-        }
-    });
-});
-
-//CREATE CANDIDATE USING THE POST ROUTE, we use post() to insert a candidate into the table 
-app.post('/api/candidate' , ({ body }, res) => { // api endpoint /api/candidate
-    const errors = inputCheck(body, 'first_name' , 'last_name' , ' industry_connected'); // we assign erros to receive the return from the inputCheck function , we will use this module ( inside here ) to verify that user info inthe request can create a candidate IN ORDER TO USE THIS FUNCTION WE MUST IMPORT IT TO THE MODULE FIRST PLACING CONST INPUTCHECK = REQUIRE('./UTILS/INPUTCHECK'); ON TOP OF FILE
-    if (errors) {
-        res.status(400).json({ error: errors });
-        return;
-    }
-
-    //add the database call 
-    const sql = `INSERT INTO candidates (first_name, last_name, industry_connected) 
-    VALUES (?,?,?,?)`;
-    const params = [body.first_name, body.last_name, body.industry_connected];
-
-    db.query(sql, params, ( err, result) => {
+                
+  db.query(sql, (err, rows) => {
     if (err) {
-        res.status(400).json({ error: err.message });
-        return;
+      res.status(500).json({ error: err.message }); // instead of an err we'll send a status code of 500 and place the err msj within JSON obj
+      return;
     }
     res.json({
-        message: 'success' , 
-        data: body
+      message: 'success', // if there is not an err the response will be sent back saying sucess
+      data: rows
     });
-
-    });
+  });
 });
 
-//UPDATE  a canditate's party
-app.put('/api/candidate/:id', (req, res) => {
-    const errors = inputCheck(req.body, 'party_id');
+// Get single candidate with party affiliation
+app.get('/api/candidate/:id', (req, res) => { //api endpoint has an :id which specify which candidate we'll select 
+  const sql = `SELECT candidates.*, parties.name 
+               AS party_name 
+               FROM candidates 
+               LEFT JOIN parties 
+               ON candidates.party_id = parties.id 
+               WHERE candidates.id = ?`; //mention id
+  const params = [req.params.id];// we'll assign the captured value populated in the re.params obj with the key id to params. Params is assigned as an array with a single element req.params.id
 
-    if (errors) {
-        res.status(400).json({ error: errors });
-        return;
-    }
-    const sql = `UPDATE candidates SET party_id = ? 
-                 WHERE id = ?`;
-    const params = [req.body.party_id, req.params.id];
-    db.query(sql, params, (err, result) => {
-      if (err) {
-        res.status(400).json({ error: err.message });
-        // check if a record was found
-      } else if (!result.affectedRows) {
-        res.json({
-          message: 'Candidate not found'
-        });
-      } else {
-        res.json({
-          message: 'success',
-          data: req.body,
-          changes: result.affectedRows
-        });
-      }
-    });
-});
-
-//CREATE A CANDIDATE 
-/*const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
-VALUES (? , ?, ?, ?)`;
-const params = [1, 'Ronald' , 'Firbank', 1];
-
-db.query(sql, params, (err, result) => {
+  db.query(sql, params, (err, row) => {// the database then query the candidates table with this id and retrive the row specififed bcuz params can be accepted in the database call as an array.
     if (err) {
-        console.log(err);
+      res.status(400).json({ error: err.message });
+      return;
     }
-    console.log(result);
+    res.json({
+      message: 'success',
+      data: row
+    });
+  });
 });
-*/
 
+// Create a candidate we use post to create or insert a new candidate into table 
+app.post('/api/candidate', ({ body }, res) => {
+  // Candidate is allowed not to be affiliated with a party
+  const errors = inputCheck(
+    body,
+    'first_name',
+    'last_name',
+    'industry_connected'
+  );
+  if (errors) {
+    res.status(400).json({ error: errors });
+    return;
+  }
+//add database call
+  const sql = `INSERT INTO candidates (first_name, last_name, industry_connected, party_id) VALUES (?,?,?,?)`;
+  const params = [
+    body.first_name,
+    body.last_name,
+    body.industry_connected,
+    body.party_id
+  ];
 
-// DEAFULT RESPONSE FOR ANY OTHER REQUEST NOT FOUND
+  db.query(sql, params, (err, result) => {
+    if (err) {
+      res.status(400).json({ error: err.message });
+      return;
+    }
+    res.json({
+      message: 'success',
+      data: body,
+      changes: result.affectedRows
+    });
+  });
+});
+
+// Update a candidate's party
+app.put('/api/candidate/:id', (req, res) => {
+  // Candidate is allowed to not have party affiliation
+  const errors = inputCheck(req.body, 'party_id');
+  if (errors) {
+    res.status(400).json({ error: errors });
+    return;
+  }
+
+  const sql = `UPDATE candidates SET party_id = ? 
+               WHERE id = ?`;
+  const params = [req.body.party_id, req.params.id];
+  db.query(sql, params, (err, result) => {
+    if (err) {
+      res.status(400).json({ error: err.message });
+      // check if a record was found
+    } else if (!result.affectedRows) {
+      res.json({
+        message: 'Candidate not found'
+      });
+    } else {
+      res.json({
+        message: 'success',
+        data: req.body,
+        changes: result.affectedRows
+      });
+    }
+  });
+});
+
+// Delete a candidate WITH API ENDPOINT 
+app.delete('/api/candidate/:id', (req, res) => { // 
+  const sql = `DELETE FROM candidates WHERE id = ?`; // we are assigning the sql to delete from candidates table , where do we want to delete it by the id
+  const params = [req.params.id];
+  db.query(sql, params, (err, result) => {
+    if (err) {
+      res.statusMessage(400).json({ error: res.message });
+    } else if (!result.affectedRows) { // if user wants to delete  a candidate that does not exist no affectedRows as a result of the delete query means that there was no candidate by that id
+      res.json({
+        message: 'Candidate not found'
+      });
+    } else {
+      res.json({
+        message: 'deleted',
+        changes: result.affectedRows,
+        id: req.params.id
+      });
+    }
+  });
+});
+
+// Get all parties
+app.get('/api/parties', (req, res) => {
+  const sql = `SELECT * FROM parties`;
+  db.query(sql, (err, rows) => {
+    if (err) {
+      res.status(500).json({ error: err.message });
+      return;
+    }
+    res.json({
+      message: 'success',
+      data: rows
+    });
+  });
+});
+
+// Get single party
+app.get('/api/party/:id', (req, res) => {
+  const sql = `SELECT * FROM parties WHERE id = ?`;
+  const params = [req.params.id];
+
+  db.query(sql, params, (err, row) => {
+    if (err) {
+      res.status(400).json({ error: err.message });
+      return;
+    }
+    res.json({
+      message: 'success',
+      data: row
+    });
+  });
+});
+
+// Delete a party
+app.delete('/api/party/:id', (req, res) => {
+  const sql = `DELETE FROM parties WHERE id = ?`;
+  const params = [req.params.id];
+
+  db.query(sql, params, (err, result) => {
+    if (err) {
+      res.status(400).json({ error: res.message });
+      // checks if anything was deleted
+    } else if (!result.affectedRows) {
+      res.json({
+        message: 'Party not found'
+      });
+    } else {
+      res.json({
+        message: 'deleted',
+        changes: result.affectedRows,
+        id: req.params.id
+      });
+    }
+  });
+});
+
+// Not Found response for unmatched routes
 app.use((req, res) => {
-    res.status(404).end();
+  res.status(404).end();
 });
 
-
-
-// add function to start the express.js server on port 3001
-app.listen(PORT, () => {
-    console.log('Server running on port ${PORT}');
+// Start server after DB connection
+db.connect(err => {
+  if (err) throw err;
+  console.log('Database connected.');
+  // add function to start the express.js server on port 3001
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
 });
