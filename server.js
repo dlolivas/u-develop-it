6d65537ec20bf81233b4dcfa5f95e5d4ed405959
Diana Olivas <dianaolivas181@gmail.com>
2021-08-10 22:59:33 -0500

    added put request

--- server.js
+++ server.js
@@ -54,6 +54,37 @@ const db = mysql.createConnection(
 });
 */
 
+//API ROUTES FOR ALL PARTIES TABLE
+app.get('/api/parties' , (req, res) => {
+    const sql = ` SELECT * FROM parties `;
+    db.query(sql, (err, rows) => {
+        if (err) {
+            res.status(500).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: 'success' , 
+            data: rows
+        });
+    });
+});
+
+
+//API ROUTES FOR  A SINGLE PARTY WITH ID PARAMETER
+app.get('/api/party/:id' , (req, res) => {
+    const sql = `SELECT * FROM parties WHERE id = ? `;
+    const params = [req.params.id];
+    db.query(sql, params, (err, row) => {
+        if (err) {
+            res.status(400).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: row
+        });
+    });
+});
 
 // get ALL CANDIDATES with get routes 
 app.get('/api/candidates' , (req, res) => {//api endpoint
@@ -74,6 +105,29 @@ app.get('/api/candidates' , (req, res) => {//api endpoint
         });
     });
 });
+
+//delete party route
+app.delete('/api/party/:id', (req, res) => {
+    const sql = `DELETE FROM parties WHERE id = ?`;
+    const params = [req.params.id];
+    db.query(sql, params, (err, result) => {
+      if (err) {
+        res.status(400).json({ error: res.message });
+        // checks if anything was deleted
+      } else if (!result.affectedRows) {
+        res.json({
+          message: 'Party not found'
+        });
+      } else {
+        res.json({
+          message: 'deleted',
+          changes: result.affectedRows,
+          id: req.params.id
+        });
+      }
+    });
+});
+
 
 
 //GET A SINGLE CANDIDATE WITH API ENDPOINT
@@ -98,6 +152,8 @@ app.get('/api/candidate/:id' , (req, res) => {//api endpoint has an :id which sp
 
     });
 });
+
+
 
 // DELETE A CANDIDATE WITH API ENDPOINT 
 app.delete('/api/candidate/:id', (req, res) => { //api endpoint
@@ -144,6 +200,35 @@ app.post('/api/candidate' , ({ body }, res) => { // api endpoint /api/candidate
         data: body
     });
 
+    });
+});
+
+//UPDATE  a canditate's party
+app.put('/api/candidate/:id', (req, res) => {
+    const errors = inputCheck(req.body, 'party_id');
+
+    if (errors) {
+        res.status(400).json({ error: errors });
+        return;
+    }
+    const sql = `UPDATE candidates SET party_id = ? 
+                 WHERE id = ?`;
+    const params = [req.body.party_id, req.params.id];
+    db.query(sql, params, (err, result) => {
+      if (err) {
+        res.status(400).json({ error: err.message });
+        // check if a record was found
+      } else if (!result.affectedRows) {
+        res.json({
+          message: 'Candidate not found'
+        });
+      } else {
+        res.json({
+          message: 'success',
+          data: req.body,
+          changes: result.affectedRows
+        });
+      }
     });
 });
 
